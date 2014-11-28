2496c9154c663f5b399f3bf00cc06ee46aa8ab79
Moritz Tenorth <knowrob@tenorth.de>
2014-11-28 16:59:30 +0100

    add dependency on knowrob_actions

--- knowrob_basics_tutorial/prolog/init.pl
+++ knowrob_basics_tutorial/prolog/init.pl
@@ -22,6 +22,7 @@
 :- register_ros_package(knowrob_vis).
 :- register_ros_package(knowrob_map_tools).
 :- register_ros_package(knowrob_map_data).
+:- register_ros_package(knowrob_actions).
 
 :- owl_parse('package://knowrob_map_data/owl/ccrl2_semantic_map.owl').
 
